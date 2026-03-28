2822836214dcd7a16b28c6d65db422e41370bfaa
kaushikdsai <thanikachalamkaushik@gmail.com>
2026-03-28 17:44:13 +0530

    fixed sync

--- backend/server.js
+++ backend/server.js
@@ -227,6 +227,8 @@ mongoose.connect(process.env.MONGO_URI,{
     console.error("MongoDB connection error:",err);
 });
 
-server.listen(5000,()=>{
+const PORT=process.env.PORT || 5000;
+
+server.listen(PORT, () => {
     console.log("Server running on port 5000");
 });
